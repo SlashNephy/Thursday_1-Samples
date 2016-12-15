23285d88d22feb763a9d851cb5a10ae22558d7ce
Nep <spica@starry.blue>
2016-12-15 15:11:30 +0900

    Add: q09c desc

--- 12-08/q09c1.c
+++ 12-08/q09c1.c
@@ -1,3 +1,8 @@
+// q09c
+// コンソール上で異なる整数n,rを入力し，順列nPrと組合せnCrを算出するプログラムを作成してください。
+// ただし、n!を計算する関数fact(n)、順列nPrを計算する関数P(n,r)、組合せnCrを計算する関数C(n,r)、入出力を行う関数main()はそれぞれ異なるファイルに保存し、分割コンパイルを行うものとします。
+// ■提出ファイル名： main()→ q09c1.c，fact(n)→q09c2.c, P(n,r)→q09c3.c, C(n,r)→p09c4.c
+
 #include <stdio.h>
 
 extern double fact(int n);
